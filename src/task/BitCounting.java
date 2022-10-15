046b11021a91c685ff56bba71fcedb5506325334
kacperchm <chmuraka1998@gmail.com>
2022-10-15 17:43:15 +0200

    Added task 'BuildAPileOfCubes' and descriptions

--- src/task/BitCounting.java
+++ src/task/BitCounting.java
@@ -1,3 +1,6 @@
+//Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary
+//representation of that number. You can guarantee that input is non-negative.
+
 package task;
 import java.lang.String;
 
